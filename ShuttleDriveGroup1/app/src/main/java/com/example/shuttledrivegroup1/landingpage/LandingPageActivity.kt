71efea19450d8bad48c74397b55a9e5ef216ea55
jeffreylarbiakor <jeffreylarbiakor@gmail.com>
2022-02-25 16:28:50 +0000

    Landing Page Activity

---
+++ ShuttleDriveGroup1/app/src/main/java/com/example/shuttledrivegroup1/landingpage/LandingPageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shuttledrivegroup1.landingpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shuttledrivegroup1.R
+
+class LandingPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing_page)
+    }
+}
